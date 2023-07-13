77d35793435f4b085c20da8e3d79e87241c3a313
Byounguk An <dbzl159753@naver.com>
2023-07-13 15:16:32 +0900

    0713

--- 연습문제/LV3/연속 펄스 부분 수열의 합.py
+++ 연습문제/LV3/연속 펄스 부분 수열의 합.py
@@ -31,3 +31,30 @@ def solution(sequence):
 
         if answer < temp:
             answer = temp
+
+### 정답
+from sys import maxsize
+
+INF = maxsize
+
+def solution(sequence):
+    answer = -INF
+    size = len(sequence)
+    s1 = s2 = 0				# 1 
+    s1_min = s2_min = 0		# 2
+    pulse = 1
+    
+    for i in range(size):
+        s1 += sequence[i] * pulse
+        s2 += sequence[i] * (-pulse)
+        
+        # 3 가장 작은 값을 빼야 큰값이 도출
+        answer = max(answer, s1-s1_min, s2-s2_min)
+        
+        # 4 
+        s1_min = min(s1_min, s1)
+        s2_min = min(s2_min, s2)
+        
+        # 5 펄스 값 바꾸기
+        pulse *= -1
+    return answer
